417d2363ebe38f44a41e08cc23ac2b62ad455d4d
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-06-26 06:22:59 -0600

    Complete initial template integration

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+
+
+
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: PagesComponent,
+        loadChildren: () => import('./childs-routes.module').then(m => m.ChildsRoutesModule)
+      }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {}
