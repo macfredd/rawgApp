a34e6b59efa01c1026131f8d16f6ced81f9765ea
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-07-24 18:52:57 -0600

    Enabling scrollPositionRestoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled'
+  }), PagesRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
