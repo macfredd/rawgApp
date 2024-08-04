19b57455d751b83ddb458a87ed942a9168c0a86c
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2024-08-03 20:02:35 -0600

    Minor style changes, start building search service

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor() { }
+
+  globalSearch(searchText: string) {
+    //https://rawg.io/api/games?page_size=20&search=Assasins&page=1&key=c542e67aec3a4340908f9de9e86038af
+    //https://rawg.io/api/collections?page_size=20&search=Assasins&page=1&key=c542e67aec3a4340908f9de9e86038af
+    //https://rawg.io/api/creators?page_size=20&search=Assasins&page=1&key=c542e67aec3a4340908f9de9e86038af
+  
+    
+  }
+}
